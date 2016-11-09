7cc52659756ec183b234ee1a7e07cf296ae37aa7
Valentyn Hrynevych <valik.intertech@gmail.com>
2016-11-09 18:17:40 +0200

    Добавлен is-active для вложеных меню

--- app/Page.php
+++ app/Page.php
@@ -8,18 +8,23 @@ use Illuminate\Support\Facades\Request;
 class Page extends Model
 {
     protected $table = 'perevorot_page_page';
+    protected $children = [];
 
     public function children()
     {
-        $pages = Page::where('nest_left', '>', $this->nest_left)
-            ->where('nest_right', '<', $this->nest_right)
-            ->where('nest_depth', $this->nest_depth + 1)
-            ->where('is_hidden', false)
-            ->where('is_disabled', false)
-            ->orderBy('nest_left')
-            ->get()
-        ;
+        if (!$this->children) {
+            $pages = Page::where('nest_left', '>', $this->nest_left)
+                ->where('nest_right', '<', $this->nest_right)
+                ->where('nest_depth', $this->nest_depth + 1)
+                ->where('is_hidden', false)
+                ->where('is_disabled', false)
+                ->orderBy('nest_left')
+                ->get()
+            ;
 
-        return Helpers::filterActivePages($pages);
+            $this->children = Helpers::filterActivePages($pages);
+        }
+
+        return $this->children;
     }
 }
