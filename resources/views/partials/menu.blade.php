eba1750d3884fb415968e65c728d3a85ca4bcc26
ira <bakum21@ukr.net>
2016-11-09 12:21:13 +0200

    Верстка выпадающего меню

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,6 +1,15 @@
 @if(!empty($menu))
-    <div class="c-header__nav-wrap">
-        <ul class="c-header__nav-list">
+    <div class="c-header__nav-wrap nav-header">
+        <ul class="c-header__nav-list menu-header">
+            <li class="parent">
+                <a href="#">Меню с вложеностью</a>
+                <ul class="drop-menu">
+                    <li><a href="#">Про мониторинг</a></li>
+                    <li><a href="#">Оскарження</a></li>
+                    <li><a href="#">ЗрадоПеремоги</a></li>
+                    <li><a href="#">Dozorro спільнота</a></li>
+                </ul>
+            </li>
             @foreach($menu as $item)
                 <li>
                     <a href="{{ \App\Helpers::getLocalizedUrl($item->url) }}"@if($item->active) class="is-active"@endif>{{ $item->title }}</a>
