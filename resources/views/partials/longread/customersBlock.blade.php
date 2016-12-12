c27c9ef80b91913a4fa5229071e370620584d2b2
Andrey Lanko <lanko@perevorot.com>
2016-12-12 19:03:15 +0000

    Правки по блоку с заказчикамиё

--- resources/views/partials/longread/customersBlock.blade.php
+++ resources/views/partials/longread/customersBlock.blade.php
@@ -1,13 +1,17 @@
 @if(!empty($block->data->customers))
     <div class="c-customers">
         <div class="container">
-            <div class="col-md-12 c-top-items__col">
-                @if(!empty($data->customers_title))
-                    <h2>{{ $data->customers_title }}</h2>
-                @endif
-                @foreach($block->data->customers as $customer)
-                    <div><a href="{{ $customer->url }}"><img src="{{ $customer->logo }}"></a></div>
-                @endforeach
+            <div class="row">
+                <div class="col-md-12 c-top-items__col">
+                    @if(!empty($data->customers_title))
+                        <h2>{{ $data->customers_title }}</h2>
+                    @endif
+                    <div class="row">
+                        @foreach($block->data->customers as $customer)
+                            <div class="col-md-3"><a href="{{ $customer->url }}"><img src="{{ $customer->logo }}" style="width:100%"></a></div>
+                        @endforeach
+                    </div>
+                </div>
             </div>
         </div>
     </div>
