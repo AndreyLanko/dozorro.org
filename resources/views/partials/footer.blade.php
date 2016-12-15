3447b862f9641e3ab5fa0d82036444b5d8240dd0
Kykylkan <kykyllkan@gmail.com>
2016-12-15 15:20:56 +0200

    add new footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,63 @@
+<div class="c-f">
+    <div class="c-f__top-row">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="row">
+                        @include('partials.bottom_menu', ['menu' => $bottom_menu])
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-12">
+
+                            <div class="sb-footer-card">
+                                <h3>Портал створено за підтримки:</h3>
+                                <div class="sb-footer-card__row">
+                                    <div class="sb-footer-card__logo-list">
+                                        <div>
+                                            <a href="#">
+                                                <img src="/assets/images/c-f/c/c-f-logo-1.png">
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="#">
+                                                <img src="/assets/images/c-f/c/c-f-logo-2.png">
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="#">
+                                                <img src="/assets/images/c-f/c/c-f-logo-3.png">
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="sb-footer-card__row sb-footer-card__row--buttons">
+                                    <div class="row">
+                                        <div class="col-md-7">
+                                            <a href="#" class="sb-footer-card__social-button sb-footer-card__social-button--tw"><span>Twitter</span></a>
+                                            <a href="#" class="sb-footer-card__social-button sb-footer-card__social-button--fb"><span>Facebook</span></a>
+                                        </div>
+                                        <div class="col-md-5">
+                                            <a href="#" class="sb-footer-card__to-dev-button"><span>Написати розробникам</span></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="c-f__bottom-row">
+        <div class="container">
+            <p class="c-f__copy">© 2016 Моніторинговий портал DoZorro. Всі права захищено</p>
+        </div>
+    </div>
+</div>
+
 <div class="c-footer">
     <div class="container">
         <div class="row">
