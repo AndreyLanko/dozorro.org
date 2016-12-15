386fae9203eb0a377fee8fb4b1dfbfb7e81024b1
Kykylkan <kykyllkan@gmail.com>
2016-12-15 16:25:25 +0200

    add longread text2

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -57,24 +57,3 @@
         </div>
     </div>
 </div>
-
-<div class="c-footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <p class="c-footer__copy">© 2016 Dozorro. Всі права захищено.</p>
-            </div>
-            <div class="col-md-9 c-footer__nav-col">
-                @if(!empty($menu))
-                    <ul class="c-footer__nav-list">
-                        @foreach($menu as $item)
-                            <li>
-                                <a href="{{ \App\Helpers::getLocalizedUrl($item->url) }}"@if($item->active) class="is-active"@endif>{{ $item->title }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                @endif
-            </div>
-        </div>
-    </div>
-</div>
