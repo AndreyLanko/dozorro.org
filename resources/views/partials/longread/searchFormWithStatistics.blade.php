1aea5da54112c12d41eede8ffae9ef6e174dd7bb
Andrey Lanko <lanko@perevorot.com>
2016-12-18 22:36:12 +0000

    Правки в блоке статистики

--- resources/views/partials/longread/searchFormWithStatistics.blade.php
+++ resources/views/partials/longread/searchFormWithStatistics.blade.php
@@ -2,29 +2,29 @@
     <div class="c-t">
         <div class="container">
 
-    <form class="sb-s" action="/tender/search/">
-        <div class="row">
-            <div class="col-md-3">
-                @if (!empty($data->search_form_title))
-                    <p class="sb-s__h">{{ $data->search_form_title }}</p>
-                @endif
-            </div>
-            <div class="col-md-6 clearfix">
-                <div class="sb-s__input sb-s__input--left">
-                    <input id="tender-number" type="text" name="tid" class="jsGetInputVal" autocomplete="off" placeholder="23552556">
-                    <div class="sb-s__or">@lang('search.or')</div>
+            <form class="sb-s" action="/tender/search/">
+                <div class="row">
+                    <div class="col-md-3">
+                        @if (!empty($data->search_form_title))
+                            <p class="sb-s__h">{{ $data->search_form_title }}</p>
+                        @endif
+                    </div>
+                    <div class="col-md-6 clearfix">
+                        <div class="sb-s__input sb-s__input--left">
+                            <input id="tender-number" type="text" name="tid" class="jsGetInputVal" autocomplete="off" placeholder="23552556">
+                            <div class="sb-s__or">@lang('search.or')</div>
+                        </div>
+                        <div class="sb-s__input sb-s__input--right">
+                            <input id="tender-customer" type="text" name="tcust" class="jsGetInputVal selectized" autocomplete="off" placeholder="@lang('search.customer')" tabindex="-1" value="" style="display: none;"><div class="selectize-control jsGetInputVal single"><div class="selectize-input items not-full"><input type="text" autocomplete="off" tabindex="" placeholder="Замовник" style="width: 75px;"></div><div class="selectize-dropdown single jsGetInputVal" style="display: none; width: 233px; top: 37px; left: 0px;"><div class="selectize-dropdown-content"></div></div></div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 clearfix">
+                        <input id="btn-find" type="submit" value="@lang('search.search_something')" disabled="">
+                    </div>
                 </div>
-                <div class="sb-s__input sb-s__input--right">
-                    <input id="tender-customer" type="text" name="tcust" class="jsGetInputVal selectized" autocomplete="off" placeholder="@lang('search.customer')" tabindex="-1" value="" style="display: none;"><div class="selectize-control jsGetInputVal single"><div class="selectize-input items not-full"><input type="text" autocomplete="off" tabindex="" placeholder="Замовник" style="width: 75px;"></div><div class="selectize-dropdown single jsGetInputVal" style="display: none; width: 233px; top: 37px; left: 0px;"><div class="selectize-dropdown-content"></div></div></div>
-                </div>
-            </div>
-            <div class="col-md-3 clearfix">
-                <input id="btn-find" type="submit" value="@lang('search.search_something')" disabled="">
-            </div>
-        </div>
-    </form>
+            </form>
 
-            @if($block->data['stats']->tenders_sum || $block->data['stats']->violation_sum || $block->data['stats']->comments || $block->data['stats']->reviews)
+            @if(!empty($block->data['stats']) && ($block->data['stats']->tenders_sum || $block->data['stats']->violation_sum || $block->data['stats']->comments || $block->data['stats']->reviews))
             <div class="row c-t__cards">
                 @if($block->data['stats']->tenders_sum)
                 <div class="col-md-3">
