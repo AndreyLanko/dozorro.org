b7d67fe1a586b3f81ed3bdd88a7db130017ce7b1
Andrey Lanko <lanko@perevorot.com>
2016-12-22 12:35:35 +0000

    Комментирование статус у актуального тендера

--- resources/views/partials/longread/actualTendersAndReviews.blade.php
+++ resources/views/partials/longread/actualTendersAndReviews.blade.php
@@ -32,32 +32,33 @@
             @endif
 
             @if(is_array($block->data['reviews']) && !empty($block->data['reviews']))
-            <div class="col-md-6">
-                <div class="c-list-card">
-                    <div class="c-list-card__inner">
-                        <h3 class="c-list-card__header">Обговорюванi тендери</h3>
-                        @foreach($block->data['reviews'] as $item)
-                            @if(isset($item->data) && $item->data)
-                                <div class="sb-list-item">
-                                    <div class="sb-list-item__row">
-                                        <h2><a href="/tender/{{ $item->data->tenderID }}/">{{ $item->data->title }}</a></h2>
-                                        @if(isset($item->data->description))<h3>{{ $item->data->description }}</h3>@endif
+                <div class="col-md-6">
+                    <div class="c-list-card">
+                        <div class="c-list-card__inner">
+                            <h3 class="c-list-card__header">Обговорюванi тендери</h3>
+                            @foreach($block->data['reviews'] as $item)
+                                @if(isset($item->data) && $item->data)
+                                    <div class="sb-list-item">
+                                        <div class="sb-list-item__row">
+                                            <h2><a href="/tender/{{ $item->data->tenderID }}/">{{ $item->data->title }}</a></h2>
+                                            @if(isset($item->data->description))<h3>{{ $item->data->description }}</h3>@endif
+                                        </div>
+                                        <div class="sb-list-item__row">
+                                            {{--<a href="#" class="sb-list-item__stat">Кваліфікація переможця</a>--}}
+                                            <a href="/tender/{{ $item->data->tenderID }}/" class="sb-list-item__comments">{{ $item->data->total_reviews }}</a>
+                                        </div>
                                     </div>
-                                    <div class="sb-list-item__row">
-                                        <a href="#" class="sb-list-item__stat">Кваліфікація переможця</a>
-                                        <a href="#" class="sb-list-item__comments">{{ $item->data->total_reviews }}</a>
-                                    </div>
-                                </div>
-                            @endif
-                        @endforeach
-                        <div class="c-list-card__link-wrap">
-                            <a href="#">Всі популярні тендери</a>
+                                @endif
+                            @endforeach
+                            {{--
+                            <div class="c-list-card__link-wrap">
+                                <a href="">Всі популярні тендери</a>
+                            </div>
+                            --}}
                         </div>
                     </div>
                 </div>
-            </div>
             @endif
-
         </div>
     </div>
 </div>
