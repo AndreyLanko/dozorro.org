4d9d344d23da4ce4e1e3ca6d451b2e3e4873ccdb
ira <bakum21@ukr.net>
2017-02-09 14:43:21 +0200

    блок на главной "Замовники, що працюють на порталі"

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,6 +8,78 @@
             'data' => $block->value
         ])
     @endforeach
+
+
+        <div class="block_customers_logo">
+            <div class="container">
+                <div class="inline-layout title_block">
+
+                        <h3>Замовники, що працюють на порталі</h3>
+
+                        <a href="#" class="all_customers">Всі замовники</a>
+
+                </div>
+
+
+                <div class="row">
+                    <div class="col-md-2">
+                        <a href="#">
+                            <div class="img-holder">
+                                <img src="/assets/images/t-logo.svg" alt="Дозорро">
+                            </div>
+                            <div class="name_customers">Дозорро</div>
+                        </a>
+
+                    </div>
+                    <div class="col-md-2">
+                        <a href="#">
+                            <div class="img-holder">
+                                <img src="/assets/images/t-logo.svg" alt="Дозорро">
+                            </div>
+                            <div class="name_customers">Дозорро</div>
+                        </a>
+
+                    </div>
+                    <div class="col-md-2">
+                        <a href="#">
+                            <div class="img-holder">
+                                <img src="/assets/images/t-logo.svg" alt="Дозорро">
+                            </div>
+                            <div class="name_customers">Дозорро</div>
+                        </a>
+
+                    </div>
+                    <div class="col-md-2">
+                        <a href="#">
+                            <div class="img-holder">
+                                <img src="/assets/images/t-logo.svg" alt="Дозорро">
+                            </div>
+                            <div class="name_customers">Дозорро</div>
+                        </a>
+
+                    </div>
+                    <div class="col-md-2">
+                        <a href="#">
+                            <div class="img-holder">
+                                <img src="/assets/images/t-logo.svg" alt="Дозорро">
+                            </div>
+                            <div class="name_customers">Дозорро</div>
+                        </a>
+
+                    </div>
+                    <div class="col-md-2">
+                        <a href="#">
+                            <div class="img-holder">
+                                <img src="/assets/images/t-logo.svg" alt="Дозорро">
+                            </div>
+                            <div class="name_customers">Дозорро</div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
 </div>
 
 @endsection
